6a6786ffd5f5841e85d97cffa7a2339ab3143996
noyoyama <noyoyama777@gmail.com>
2025-08-27 04:12:19 +0000

    ピンをマウスオーバーで画像表示、ピンをクリック時にスクロール

--- app/controllers/onsens_controller.rb
+++ app/controllers/onsens_controller.rb
@@ -6,7 +6,20 @@ class OnsensController < ApplicationController
     @search_params = params.permit(:q, :tags)
 
     # モデルの検索メソッドを呼び出し、新しい順でソート
+    #     onsensテーブルのカラムに対応付けた情報すべてを@Onsens(要するにグローバル変数)にぶちこむ
+    #     ので、onsens.nameで名前、onsens.idでid、というように情報を取り出せる(らしい)
+    #     でも画像はただの文字データではないのでonsen.imageとやってもデータを持ってこられるわけではない(Active Storageを使わなきゃ)
     @onsens = Onsen.search(@search_params).order(created_at: :desc)
+
+    @onsens_for_map = @onsens.map do |onsen|
+      onsen.as_json.merge(
+        image_url: if onsen.images.attached?
+                     url_for(onsen.images.first.representation(resize_to_limit: [100, 100]))
+                   else
+                     nil
+                   end
+      )
+    end
   end
 
   # GET /onsens/1 or /onsens/1.json
---
+++ db/migrate/20250826060316_add_published_to_onsens.rb
@@ -0,0 +1,4 @@
+class AddPublishedToOnsens < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
